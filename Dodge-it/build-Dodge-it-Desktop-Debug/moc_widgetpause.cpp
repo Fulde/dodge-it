ac85328e6d1e9fe44ca391054b0e686e79e2c948
slaird5 <slaird5@gmail.com>
2014-03-10 10:50:17 -0400

    stephen

--- Dodge-it/build-Dodge-it-Desktop-Debug/moc_widgetpause.cpp
+++ Dodge-it/build-Dodge-it-Desktop-Debug/moc_widgetpause.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_WidgetPause_t {
-    QByteArrayData data[1];
-    char stringdata[13];
+    QByteArrayData data[5];
+    char stringdata[74];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -29,9 +29,14 @@ struct qt_meta_stringdata_WidgetPause_t {
     )
 static const qt_meta_stringdata_WidgetPause_t qt_meta_stringdata_WidgetPause = {
     {
-QT_MOC_LITERAL(0, 0, 11)
+QT_MOC_LITERAL(0, 0, 11),
+QT_MOC_LITERAL(1, 12, 20),
+QT_MOC_LITERAL(2, 33, 0),
+QT_MOC_LITERAL(3, 34, 19),
+QT_MOC_LITERAL(4, 54, 18)
     },
-    "WidgetPause\0"
+    "WidgetPause\0on_btnResume_clicked\0\0"
+    "on_btnCheat_clicked\0on_btnSave_clicked\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -41,21 +46,37 @@ static const uint qt_meta_data_WidgetPause[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   29,    2, 0x08,
+       3,    0,   30,    2, 0x08,
+       4,    0,   31,    2, 0x08,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void WidgetPause::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        WidgetPause *_t = static_cast<WidgetPause *>(_o);
+        switch (_id) {
+        case 0: _t->on_btnResume_clicked(); break;
+        case 1: _t->on_btnCheat_clicked(); break;
+        case 2: _t->on_btnSave_clicked(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -83,6 +104,15 @@ int WidgetPause::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QWidget::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
