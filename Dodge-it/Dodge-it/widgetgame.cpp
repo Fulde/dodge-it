c87c0b9c7f868c14dece03bbdbe6b191be83b0bd
Fulde <daniel.fulde@outlook.com>
2014-03-11 10:46:26 -0400

    arrow keys now work for movement

--- Dodge-it/Dodge-it/widgetgame.cpp
+++ Dodge-it/Dodge-it/widgetgame.cpp
@@ -5,6 +5,8 @@
 //==============================
 #include <QTimer>
 #include <QLabel>
+#include <QKeyEvent>
+#include <QEvent>
 
 #include "widgetgame.h"
 #include "ui_widgetgame.h"
@@ -19,6 +21,8 @@ WidgetGame::WidgetGame(QWidget *parent) :
     //Set up the ui for the widget
     ui->setupUi(this);
 
+    setFocusPolicy(Qt::StrongFocus);
+
     timer = new QTimer();
     timer->setInterval(Game::getInstance().getInterval());
     connect(timer, &QTimer::timeout, this, &WidgetGame::timerHit);
@@ -28,6 +32,27 @@ WidgetGame::WidgetGame(QWidget *parent) :
 //Destructor for Widget
 WidgetGame::~WidgetGame() {
     delete ui;
+}
+
+
+void WidgetGame::keyPressEvent(QKeyEvent *k)
+{
+    if (k->key() == Qt::Key_Up)
+    {
+        ui->lblSatyr->move(ui->lblSatyr->x(), ui->lblSatyr->y() - 10);
+    }
+    else if (k->key() == Qt::Key_Down)
+    {
+        ui->lblSatyr->move(ui->lblSatyr->x(), ui->lblSatyr->y() + 10);
+    }
+    else if (k->key() == Qt::Key_Left)
+    {
+        ui->lblSatyr->move(ui->lblSatyr->x() - 10, ui->lblSatyr->y());
+    }
+    else if (k->key() == Qt::Key_Right)
+    {
+        ui->lblSatyr->move(ui->lblSatyr->x() + 10, ui->lblSatyr->y());
+    }
 }
 
 
